6c08a8ad1e6dc037c6fe0c1b81dd3ee1eefb451e
sgupta82297 <47404972+sgupta82297@users.noreply.github.com>
2019-04-21 22:57:01 -0400

    Create tasklist.js

---
+++ src/tasklist.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TaskItem.js from './TaskItem.js';
+class TaskList extends React.Component {
+markDone = (task) => {
+let taskList = this.props.tasks;
+taskList.(taskIndex);
+onUpdateTaskList(taskList);
+Array.prototype.mfilter = function (){
+var filtered = [];
+for(TaskItem.length +1){
+filtered.push(to do[i]){
+filtered.push(completed[i]){
+filtered.push(pending[i]){
+}
+render() {
+return <TaskItem task={task} key={task.id} markDone={this.markDone} />
+return (
+<className="taskList">
+{TaskItem}
+</ul>
+)
